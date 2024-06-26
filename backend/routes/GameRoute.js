f437f4d224da85a29c90e9bd8480e11f684f1c20
Rajatkhanuja <rajatkhanuja81@gmail.com>
2024-06-26 11:25:31 +0530

    routes

--- backend/routes/GameRoute.js
+++ backend/routes/GameRoute.js
@@ -1,1 +1,13 @@
 import express from "express"
+import { addGame } from "../controllers/GameController.js"
+import multer from "multer"
+
+const GameRouter = express.Router();
+
+
+
+
+
+
+
+export default GameRouter;
